c47e0dc1dcd723801a6e015e0b2012247b7f4a2d
Ola Olsson <12532102+saolaolsson@users.noreply.github.com>
2024-12-11 08:21:35 +0100

    Improve variable names

--- 2024/aoc_2024_08.cpp
+++ 2024/aoc_2024_08.cpp
@@ -11,31 +11,34 @@
 
 int main() {
   const Grid grid{std::cin};
-  std::map<char, std::vector<Vector2i>> antennae;
+  std::map<char, std::vector<Vector2i>> all_freq_antennae;
   for (auto grid_it = grid.cbegin(); grid_it != grid.cend(); ++grid_it) {
     if (std::isalnum(*grid_it)) {
-      antennae[*grid_it].push_back(grid_it);
+      all_freq_antennae[*grid_it].push_back(grid_it);
     }
   }
 
   std::set<Vector2i> antinodes;
   std::set<Vector2i> multi_antinodes;
-  for (const auto& antenna : antennae) {
-    for (const auto& [antenna_a, antenna_b] :
-         std::views::cartesian_product(antenna.second, antenna.second)) {
-      if (antenna_a == antenna_b) {
+  for (const auto& single_freq_antennae : all_freq_antennae) {
+    const auto& anntennae_locations = single_freq_antennae.second;
+    for (const auto& [antenna_location_a, antenna_location_b] :
+         std::views::cartesian_product(anntennae_locations,
+                                       anntennae_locations)) {
+      if (antenna_location_a == antenna_location_b) {
         continue;
       }
-      auto antinode = antenna_a;
-      const auto delta = antenna_b - antenna_a;
+      auto antinode_location = antenna_location_a;
+      const auto antenna_location_delta =
+          antenna_location_b - antenna_location_a;
 
-      if (grid.is_inside(antinode - delta)) {
-        antinodes.insert(antinode - delta);
+      if (grid.is_inside(antinode_location - antenna_location_delta)) {
+        antinodes.insert(antinode_location - antenna_location_delta);
       }
 
-      while (grid.is_inside(antinode)) {
-        multi_antinodes.insert(antinode);
-        antinode -= delta;
+      while (grid.is_inside(antinode_location)) {
+        multi_antinodes.insert(antinode_location);
+        antinode_location -= antenna_location_delta;
       }
     }
   }
